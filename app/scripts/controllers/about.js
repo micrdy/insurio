223a1c86446d6fb7bf6f00ee5297d1556018438f
micrdy <WEMAKO@Lukass-iMac.lan>
2015-08-19 14:00:40 +0200

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name insurioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the insurioApp
+ */
+angular.module('insurioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name insurioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the insurioApp
+ */
+angular.module('insurioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
